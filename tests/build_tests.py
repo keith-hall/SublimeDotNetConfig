e805965e3d69f6dbbcb397ad124244ece940ddf5
Keith Hall <kingkeith+github@gmail.com>
2018-01-17 17:15:19 +0200

    update tests

--- tests/build_tests.py
+++ tests/build_tests.py
@@ -3,11 +3,27 @@
 # and then use the `UnitTesting: Test Current File` entry in the command palette
 
 import unittest
-from SublimeDotNetConfig.build import find_base_file
+from SublimeDotNetConfig.build import find_base_file, is_base_file
 
 class TestFindBaseFile(unittest.TestCase):
     def test_web_rc(self):
         self.assertEqual(find_base_file('Web.Release Candidate.config'), 'Web.config')
 
     def test_app(self): # TODO: make it possible to test falling back to `App.config` if the file with the same name as the executable doesn't exist
-        self.assertEqual(find_base_file('Example.Application.Name.exe.config'), 'Example.Application.Name.config')
+        self.assertEqual(find_base_file('Example.Application.Name.exe.Some-Transformation.config'), 'Example.Application.Name.exe.config')
+
+class TestIsBaseFile(unittest.TestCase):
+    def test_web_rc(self):
+        self.assertFalse(is_base_file('Web.Release Candidate.config'))
+
+    def test_web(self):
+        self.assertTrue(is_base_file('Web.config'))
+
+    def test_appexe(self):
+        self.assertTrue(is_base_file('Example.Application.Name.exe.config'))
+
+    def test_appexe_transform(self):
+        self.assertFalse(is_base_file('Example.Application.Name.exe.Some-Transformation.config'))
+
+    def test_app(self):
+        self.assertTrue(is_base_file('App.config'))
