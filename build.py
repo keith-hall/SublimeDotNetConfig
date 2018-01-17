b904aaee38c3d45da6b078b40f32f47c069d6af3
Keith Hall <kingkeith+github@gmail.com>
2018-01-17 17:03:51 +0200

    add command palette entry to switch to the base XML file

--- build.py
+++ build.py
@@ -57,16 +57,12 @@ def build_project(window, **kwargs):
     window.run_command('exec_show_output', { 'cmd': cmd, 'working_dir': path, 'action': 'xdt-build_project', 'args': kwargs })
 
 def do_transform(window, base_file, transformation_file, verbose = False):
-    if transformation_file and not base_file:
-        base_file = find_base_file(transformation_file)
+    def log_action(data):
         if verbose:
-            window.run_command('exec_data_received', { 'data': 'File to transform not specified, trying "{}"...\n'.format(base_file), 'action': 'xdt-do_transform' })
-        # if the inferred base file doesn't exist, fall back to `App.config`
-        if not os.path.isfile(base_file):
-            base_file = os.path.join(os.path.dirname(transformation_file), 'App.config')
-            if verbose:
-                window.run_command('exec_data_received', { 'data': 'File to transform not found, falling back to "{}"...\n'.format(base_file), 'action': 'xdt-do_transform' })
-    
+            window.run_command('exec_data_received', { 'data': data + '\n', 'action': 'xdt-do_transform' })
+    if transformation_file and not base_file:
+        base_file = get_base_file(transformation_file, log_action)
+
     path = cache_path()
     output_file = os.path.join(path, 'transformed.config')
     # perform the transformation
@@ -74,6 +70,15 @@ def do_transform(window, base_file, transformation_file, verbose = False):
     if verbose:
         cmd.append('-v')
     window.run_command('exec_show_output', { 'cmd': cmd, 'working_dir': path, 'action': 'xdt-do_transform', 'args': { 'open': output_file } })
+
+def get_base_file(transformation_file, log_action):
+    base_file = find_base_file(transformation_file)
+    log_action('File to transform not specified, trying "{}"...'.format(base_file))
+    # if the inferred base file doesn't exist, fall back to `App.config`
+    if not os.path.isfile(base_file):
+        base_file = os.path.join(os.path.dirname(transformation_file), 'App.config')
+        log_action('File to transform not found, falling back to "{}"...'.format(base_file))
+    return base_file
 
 def find_base_file(transformation_file):
     """Given the full path to the file that contains the transformation to apply, guess the filename of the base file to be transformed, and return the path to it."""
@@ -104,7 +109,9 @@ class TransformationListener(sublime_plugin.EventListener):
             action = args['action'][len('xdt-'):]
             if action == 'do_transform':
                 path = args['args']['open']
+                # if the output file exists (NOTE: we could also maybe check the exit_code)
                 if os.path.isfile(path):
+                    # open it in ST (note that our on_load hook will fire)
                     window.open_file(path)
             elif action == 'create_project':
                 build_project(window, **args['args'])
@@ -116,3 +123,26 @@ def after_transformed_file_loaded(view):
     view.retarget('')
     # pretty print the file - this currently uses https://packagecontrol.io/packages/IndentX if it is installed
     view.run_command('basic_indent')
+
+def is_base_file(path):
+    file_name = os.path.basename(path)
+    return file_name.count('.') == 1 or file_name.endswith('.exe.config')
+
+class XdtOpenBaseFile(sublime_plugin.WindowCommand):
+    def run(self):
+        path = self.window.active_view().file_name()
+        # NOTE: code below commented because situation cannot occur due to `is_enabled` check
+        # if is_base_file(path):
+        #     window.status_message('This is already the base file')
+        # else:
+        actions_taken = list()
+        def log_action(data):
+            actions_taken.append(data)
+        base_file = get_base_file(path, log_action)
+        # TODO: display the actions taken somewhere? log them (properly - i.e. with a package prefix and maybe even a timestamp) to the console?
+        self.window.open_file(base_file)
+    
+    def is_enabled(self):
+        view = self.window.active_view()
+        path = view.file_name()
+        return path and not is_base_file(path) and view.match_selector(0, 'text.xml.dotnetconfig')
